9901a1a8fcdaa9269a5026debd4329287549d9ca
Ayyappa359 <ayyappakomala5@gmail.com>
2026-07-15 18:13:04 +0530

    updated sql codes

---
+++ Week 1/PLSQL programming/Stored Procedures/Scenario1.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
+    UPDATE Accounts
+    SET Balance = Balance + (Balance * 0.01)
+    WHERE AccountType = 'Savings';
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Monthly interest applied successfully.');
+END;
+/
+
+-- Execute the procedure
+BEGIN
+    ProcessMonthlyInterest;
+END;
+/
