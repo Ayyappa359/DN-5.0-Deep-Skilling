d56ae53a1943625d18afe24bcdad4303da148e61
Ayyappa359 <ayyappakomala5@gmail.com>
2026-07-16 15:08:08 +0530

    Updated week 3 codes

---
+++ week 3/Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.cognizant.springlearn.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager users() {
+
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("pwd"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("pwd"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/authenticate").hasAnyRole("USER","ADMIN")
+                    .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
